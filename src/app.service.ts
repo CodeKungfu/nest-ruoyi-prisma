725e61e951aff76c3aca412346f2d3f029eec7bf
keming <likeming@hqz.net.cn>
2023-12-01 16:45:32 +0800

    wip: first api

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): any {
+    return {"msg":"操作成功","code":200,"captchaEnabled":false};
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import { setupSwagger } from './setup-swagger';
 
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.SERVER_PORT || 7071;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // setupSwagger(app);
   await app.listen(SERVER_PORT, '0.0.0.0');
   const serverUrl = await app.getUrl();
   Logger.log(`api服务已经启动,请访问: ${serverUrl}`);
